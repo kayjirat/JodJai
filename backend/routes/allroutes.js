877a950c104406165c1106cb71dd9e671561fa96
opnxjm <onparin.jitm@mail.kmutt.ac.th>
2024-06-11 15:12:29 +0700

    [ADD] goals and subgoals

--- backend/routes/allroutes.js
+++ backend/routes/allroutes.js
@@ -1,12 +1,15 @@
 const authen = require('./authen');
+const goal = require('./goal');
+const subgoal = require('./subgoal');
 
 class Routes {
     constructor(app) {
         this.app = app;
     }
-
     getRoutes() {
         this.app.use('/api/authen', authen);
+        this.app.use('/api/goal', goal);
+        this.app.use('/api/subgoal', subgoal);
     }
 }
 
