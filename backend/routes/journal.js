dfe6b82dbcdf5d6bbe325313bc7d6118f0291832
opnxjm <onparin.jitm@mail.kmutt.ac.th>
2024-06-16 22:53:14 +0700

    [ADD] journals and users

---
+++ backend/routes/journal.js
@@ -0,0 +1,278 @@
+const router = require('express').Router();
+const sql = require('mssql');
+const moment = require('moment-timezone');
+const verifyJWT = require('../middlewares/midauthen');
+require('dotenv').config();
+
+const dbConfig = {
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    server: process.env.DB_SERVER,
+    database: process.env.DB_DATABASE,
+    options: {
+        encrypt: true,
+        enableArithAbort: true
+    }
+};
+
+function getCurrentDateTimeUTC7() {
+    const dateTime = moment();
+    const dateTimeUTC7 = dateTime.tz('Asia/Bangkok');
+    return dateTimeUTC7.format('YYYY-MM-DDTHH:mm:ss.SSS') + 'Z';
+}
+
+router.post('/create', verifyJWT, async (req, res) => {
+    const user_id = req.user.user_id;
+    const { title, content, entry_date, mood_rating } = req.body;
+    const created_at = getCurrentDateTimeUTC7();
+    const updated_at = created_at;
+    if (!user_id || !title || !content || !entry_date || !mood_rating) {
+        return res.status(400).send('Bad Request');
+    }
+    try {
+        const pool = await sql.connect(dbConfig);
+        const result = await pool.request()
+            .input('user_id', sql.Int, user_id)
+            .input('title', sql.NVarChar, title)
+            .input('content', sql.NText, content)
+            .input('entry_date', sql.Date, entry_date)
+            .input('mood_rating', sql.Int, mood_rating)
+            .input('created_at', sql.DateTime, created_at)
+            .input('updated_at', sql.DateTime, updated_at)
+            .query('INSERT INTO Journals (user_id, title, content, entry_date, mood_rating, created_at, updated_at) VALUES (@user_id, @title, @content, @entry_date, @mood_rating, @created_at, @updated_at)');
+
+        res.status(201).send('Journal entry created');
+    } catch (err) {
+        console.error('SQL error', err);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+router.get('/journal/:id', verifyJWT, async (req, res) => {
+    const user_id = req.user.user_id;
+    if (!user_id) {
+        return res.status(400).send('Bad Request');
+    }
+    try {
+        const pool = await sql.connect(dbConfig);
+        const result = await pool.request()
+            .input('user_id', sql.Int, user_id)
+            .input('journal_id', sql.Int, req.params.id)
+            .query('SELECT * FROM Journals WHERE journal_id = @journal_id AND user_id = @user_id');
+        if (result.recordset.length === 0) {
+            return res.status(404).send('Journal entry not found');
+        }
+        res.status(200).json(result.recordset[0]);
+    } catch (err) {
+        console.error('SQL error', err);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+router.patch('/update/:id', verifyJWT, async (req, res) => {
+    const user_id = req.user.user_id;
+    const { title, content, entry_date, mood_rating } = req.body;
+    const id = req.params.id;
+    console.log(entry_date);
+    const updated_at = getCurrentDateTimeUTC7();
+    if (!user_id || !title || !content || !entry_date || !mood_rating) {
+        return res.status(400).send('Bad Request');
+    }
+    try {
+        const pool = await sql.connect(dbConfig);
+        const result = await pool.request()
+            .input('user_id', sql.Int, user_id)
+            .input('title', sql.NVarChar, title)
+            .input('content', sql.NText, content)
+            .input('entry_date', sql.Date, entry_date)
+            .input('mood_rating', sql.Int, mood_rating)
+            .input('updated_at', sql.DateTime, updated_at)
+            .input('journal_id', sql.Int, id)
+            .query('UPDATE Journals SET title = @title, content = @content, entry_date = @entry_date, mood_rating = @mood_rating, updated_at = @updated_at WHERE journal_id = @journal_id AND user_id = @user_id');
+        if (result.rowsAffected[0] === 0) {
+            return res.status(404).send('Journal entry not found');
+        }
+        res.status(200).send('Journal entry updated');
+    } catch (err) {
+        console.error('SQL error', err);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+router.delete('/delete/:id', verifyJWT, async (req, res) => {
+    const user_id = req.user.user_id;
+    if (!user_id) {
+        return res.status(400).send('Bad Request');
+    }
+    try {
+        const pool = await sql.connect(dbConfig);
+        const result = await pool.request()
+            .input('user_id', sql.Int, user_id)
+            .input('journal_id', sql.Int, req.params.id)
+            .query('DELETE FROM Journals WHERE journal_id = @journal_id AND user_id = @user_id');
+        if (result.rowsAffected[0] === 0) {
+            return res.status(404).send('Journal entry not found');
+        }
+        res.status(200).send('Journal entry deleted');
+    } catch (err) {
+        console.error('SQL error', err);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+router.get('/list', verifyJWT, async (req, res) => {
+    const user_id = req.user.user_id;
+    console.log(user_id);
+    if (!user_id) {
+        return res.status(400).send('Bad Request');
+    }
+    try {
+        const pool = await sql.connect(dbConfig);
+        const result = await pool.request()
+            .input('user_id', sql.Int, user_id)
+            .query('SELECT * FROM Journals WHERE user_id = @user_id');
+        res.status(200).json(result.recordset);
+    } catch (err) {
+        console.error('SQL error', err);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+router.get('/ByMonthAndYear/:month/:year', verifyJWT, async (req, res) => {
+    const user_id = req.user.user_id;
+    const month = parseInt(req.params.month);
+    const year = parseInt(req.params.year);
+    if (!user_id || !month || !year) {
+        return res.status(400).send('Bad Request');
+    }
+    try {
+        const pool = await sql.connect(dbConfig);
+        const result = await pool.request()
+            .input('user_id', sql.Int, user_id)
+            .input('month', sql.Int, month)
+            .input('year', sql.Int, year)
+            .query('SELECT * FROM Journals WHERE user_id = @user_id AND MONTH(entry_date) = @month AND YEAR(entry_date) = @year ORDER BY entry_date ASC');
+        if (result.recordset.length === 0) {
+            return res.status(404).send('Journal entry not found');
+        }
+        res.status(200).json(result.recordset);
+    } catch (err) {
+        console.error('SQL error', err);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+router.get('/weeklysum', verifyJWT, async (req, res) => {
+    const user_id = req.user.user_id;
+    if (!user_id) {
+        return res.status(400).send('Bad Request');
+    }
+    try {
+        const pool = await sql.connect(dbConfig);
+        const result = await pool.request()
+            .input('user_id', sql.Int, user_id)
+            .query(`
+                WITH MoodCounts AS (
+                    SELECT 
+                        DATEPART(YEAR, entry_date) AS year,
+                        DATEPART(WEEK, entry_date) AS week,
+                        mood_rating,
+                        COUNT(*) AS mood_count
+                    FROM 
+                        Journals
+                    WHERE 
+                        user_id = @user_id
+                    GROUP BY 
+                        DATEPART(YEAR, entry_date),
+                        DATEPART(WEEK, entry_date),
+                        mood_rating
+                ),
+                MoodTotals AS (
+                    SELECT
+                        year,
+                        week,
+                        SUM(mood_count) AS total_count
+                    FROM
+                        MoodCounts
+                    GROUP BY
+                        year,
+                        week
+                )
+                SELECT 
+                    mc.year,
+                    mc.week,
+                    mc.mood_rating,
+                    mc.mood_count,
+                    ROUND((mc.mood_count * 100.0 / mt.total_count), 2) AS mood_percentage
+                FROM 
+                    MoodCounts mc
+                    JOIN MoodTotals mt
+                    ON mc.year = mt.year AND mc.week = mt.week
+                ORDER BY 
+                    mc.year, mc.week, mc.mood_rating
+            `);
+
+        res.status(200).json(result.recordset);
+    } catch (err) {
+        console.error('SQL error', err);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+router.get('/current-weeklysum', verifyJWT, async (req, res) => {
+    const user_id = req.user.user_id;
+    if (!user_id) {
+        return res.status(400).send('Bad Request');
+    }
+
+    try {
+        const pool = await sql.connect(dbConfig);
+        const result = await pool.request()
+            .input('user_id', sql.Int, user_id)
+            .query(`
+                DECLARE @startOfWeek DATE;
+                DECLARE @endOfWeek DATE;
+
+                SET @startOfWeek = DATEADD(DAY, 1 - DATEPART(WEEKDAY, GETDATE()), CAST(GETDATE() AS DATE));
+                SET @endOfWeek = DATEADD(DAY, 7 - DATEPART(WEEKDAY, GETDATE()), CAST(GETDATE() AS DATE));
+
+                WITH MoodCounts AS (
+                    SELECT 
+                        mood_rating,
+                        COUNT(*) AS mood_count
+                    FROM 
+                        Journals
+                    WHERE 
+                        user_id = @user_id
+                        AND entry_date >= @startOfWeek
+                        AND entry_date < @endOfWeek
+                    GROUP BY 
+                        mood_rating
+                ),
+                TotalCount AS (
+                    SELECT 
+                        SUM(mood_count) AS total_count
+                    FROM 
+                        MoodCounts
+                )
+                SELECT 
+                    mc.mood_rating,
+                    mc.mood_count,
+                    ROUND((mc.mood_count * 100.0 / tc.total_count), 2) AS mood_percentage
+                FROM 
+                    MoodCounts mc
+                CROSS JOIN 
+                    TotalCount tc
+                ORDER BY 
+                    mc.mood_rating
+            `);
+
+        res.status(200).json(result.recordset);
+    } catch (err) {
+        console.error('SQL error', err);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+
+module.exports = router;
---
+++ backend/routes/user.js
@@ -0,0 +1,155 @@
+const router = require('express').Router();
+const sql = require('mssql');
+const moment = require('moment-timezone');
+const verifyJWT = require('../middlewares/midauthen');
+require('dotenv').config();
+
+const dbConfig = {
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    server: process.env.DB_SERVER,
+    database: process.env.DB_DATABASE,
+    options: {
+        encrypt: true,
+        enableArithAbort: true
+    }
+};
+
+function getCurrentDateTimeUTC7() {
+    const dateTime = moment();
+    const dateTimeUTC7 = dateTime.tz('Asia/Bangkok');
+    return dateTimeUTC7.format('YYYY-MM-DDTHH:mm:ss.SSS') + 'Z';
+}
+
+router.get('/profile', verifyJWT, async (req, res) => {
+    const user_id = req.user.user_id;
+    if (!user_id) {
+        return res.status(400).send('Bad Request');
+    }
+    try {
+        const pool = await sql.connect(dbConfig);
+        const result = await pool.request()
+            .input('user_id', sql.Int, user_id)
+            .query('SELECT * FROM Users WHERE user_id = @user_id');
+        res.status(200).json(result.recordset[0]);
+    } catch (err) {
+        console.error('SQL error', err);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+router.get('/check', verifyJWT, async (req, res) => {
+    const user_id = req.user.user_id;
+    if (!user_id) {
+        return res.status(400).send('Bad Request');
+    }
+    try {
+        const pool = await sql.connect(dbConfig);
+        const result = await pool.request()
+            .input('user_id', sql.Int, user_id)
+            .query('SELECT member_status, DATEDIFF(day, created_at, GETDATE()) as account_age FROM Users WHERE user_id = @user_id');
+        
+        if (result.recordset.length === 0) {
+            return res.status(404).send('Membership information not found');
+        }
+        
+        const membershipStatus = result.recordset[0].member_status;
+        const accountAgeDays = result.recordset[0].account_age;
+        console.log(membershipStatus)
+        if ((membershipStatus === true && accountAgeDays <= 3)) {
+            return res.status(200).json(true); 
+        }
+        res.status(200).json(false);
+    } catch (err) {
+        console.error('SQL error', err);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+router.get('/membership', verifyJWT, async (req, res) => {
+    const user_id = req.user.user_id;
+    if (!user_id) {
+        return res.status(400).send('Bad Request');
+    }
+    try {
+        const pool = await sql.connect(dbConfig);
+        const result = await pool.request()
+            .input('user_id', sql.Int, user_id)
+            .query('SELECT member_status FROM Memberships WHERE user_id = @user_id');
+        res.status(200).json(result.recordset[0]);
+    } catch (err) {
+        console.error('SQL error'                                                                                                       , err);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+router.post('/membership', verifyJWT, async (req, res) => {
+    const user_id = req.user.user_id;
+    const {stripe_payment_id} = req.body;
+    const created_at = getCurrentDateTimeUTC7();
+    const memberstatus = 1;
+    if (!user_id || !stripe_payment_id) {
+        return res.status(400).send('Bad Request');
+    }
+    try {
+        const pool = await sql.connect(dbConfig);
+        const result = await pool.request()
+            .input('user_id', sql.Int, user_id)
+            .input('stripe_payment_id', sql.NVarChar, stripe_payment_id)
+            .input('created_at', sql.DateTime, created_at)
+            .input('memberstatus', sql.Int, memberstatus)
+            .query('INSERT INTO Memberships (user_id, stripe_payment_id, created_at, memberstatus) VALUES (@user_id, @stripe_payment_id, @created_at, @memberstatus)');
+        res.status(201).send('Membership created');
+    } catch (err) {
+        console.error('SQL error', err);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+router.patch('/edit', verifyJWT, async (req, res) => {
+    const user_id = req.user.user_id;
+    const { username } = req.body;
+    if (!user_id || !username) {
+        return res.status(400).send('Bad Request');
+    }
+    try {
+        const pool = await sql.connect(dbConfig);
+        const existedUsername = await pool.request()
+            .input('username', sql.NVarChar, username)
+            .query('SELECT * FROM Users WHERE username = @username');
+        if (existedUsername.recordset.length > 0) {
+            return res.status(409).send('Username already exists');
+        }
+        await pool.request()
+            .input('user_id', sql.Int, user_id)
+            .input('username', sql.NVarChar, username)
+            .query('UPDATE Users SET username = @username WHERE user_id = @user_id');
+        res.status(200).send('User updated');
+    } catch (err) {
+        console.error('SQL error', err);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+router.post('/feedback', verifyJWT, async (req, res) => {
+    const user_id = req.user.user_id;
+    const { feedback } = req.body;
+    const created_at = getCurrentDateTimeUTC7();
+    if (!user_id || !feedback) {
+        return res.status(400).send('Bad Request');
+    }
+    try {
+        const pool = await sql.connect(dbConfig);
+        await pool.request()
+            .input('user_id', sql.Int, user_id)
+            .input('feedback', sql.NVarChar, feedback)
+            .input('created_at', sql.DateTime, created_at)
+            .query('INSERT INTO Feedbacks (user_id, feedback, created_at) VALUES (@user_id, @feedback, @created_at)');
+        res.status(201).send('Feedback submitted');
+    } catch (err) {
+        console.error('SQL error', err);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+module.exports = router;
