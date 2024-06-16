e90fedff5ec65fd3343790571f1f0acb31aad6ca
opnxjm <onparin.jitm@mail.kmutt.ac.th>
2024-06-16 23:11:35 +0700

    [ADD] all journal pages

---
+++ frontend/lib/pages/journal_detail.dart
@@ -0,0 +1,304 @@
+import 'package:flutter/material.dart';
+import 'package:frontend/services/journal_service.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class JournalDetailPage extends StatefulWidget {
+  const JournalDetailPage({Key? key}) : super(key: key);
+
+  @override
+  State<JournalDetailPage> createState() => _JournalDetailPageState();
+}
+
+class _JournalDetailPageState extends State<JournalDetailPage> {
+  late JournalService _journalService;
+  late String title = '';
+  late String content = '';
+  late String date = '';
+  late int moodRating = 0;
+  late int id = 0;
+
+  bool _isLoading = true;
+  String _token = '';
+
+  @override
+  void initState() {
+    super.initState();
+    _journalService = JournalService();
+    _loadTokenAndFetchJournalEntry();
+  }
+
+  Future<void> _loadTokenAndFetchJournalEntry() async {
+    await _loadTokenFromSharedPreferences();
+    final Map<String, dynamic> journalDetails =
+        ModalRoute.of(context)!.settings.arguments as Map<String, dynamic>;
+    setState(() {
+      title = journalDetails['title'];
+      content = journalDetails['content'];
+      date = journalDetails['entryDate'];
+      moodRating = journalDetails['moodRating'];
+      id = journalDetails['id']; 
+      _isLoading = false;
+    });
+  }
+
+  String formatDate(String date) {
+    DateTime dateTime = DateTime.parse(date);
+    date = '${dateTime.day}/${dateTime.month}/${dateTime.year}';
+    return date;
+  }
+
+  Future<void> _loadTokenFromSharedPreferences() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    _token = prefs.getString('token') ?? '';
+  }
+
+  Future<void> _deleteJournalEntry() async {
+    await _journalService.deleteJournalEntry(_token, id);
+  }
+
+  String getMoodImage(int moodRating) {
+    switch (moodRating) {
+      case 1:
+        return 'assets/images/happy.png';
+      case 2:
+        return 'assets/images/smile_pink.png';
+      case 3:
+        return 'assets/images/soso.png';
+      case 4:
+        return 'assets/images/sad.png';
+      case 5:
+        return 'assets/images/cry.png';
+      default:
+        return 'assets/images/happy.png'; 
+    }
+  }
+
+  void _showDeleteDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(30.0),
+          ),
+          title: const Text(
+            'Delete Journal',
+            style: TextStyle(
+              fontFamily: 'Nunito',
+              fontSize: 28,
+              fontWeight: FontWeight.w600,
+              color: Color(0xff666159),
+            ),
+          ),
+          content: const Text(
+            'Are you sure you want to delete this journal?',
+            style: TextStyle(
+              fontFamily: 'Inter',
+              fontSize: 14,
+              color: Color(0xff666159),
+            ),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text(
+                'Cancel',
+                style: TextStyle(
+                  fontFamily: 'Inter',
+                  fontSize: 16,
+                  color: Color(0xFF9B968E),
+                ),
+              ),
+              style: TextButton.styleFrom(
+                backgroundColor: Colors.white,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                  side: const BorderSide(
+                    color: Color(0xFF9B968E),
+                    width: 1,
+                  ),
+                ),
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 20,
+                  vertical: 10,
+                ),
+              ),
+            ),
+            TextButton(
+              onPressed: () {
+                _deleteJournalEntry();
+                Navigator.of(context).pop();
+                Navigator.pushNamed(context, '/journal');
+              },
+              child: const Text(
+                'Delete',
+                style: TextStyle(
+                  fontFamily: 'Inter',
+                  fontSize: 16,
+                  color: Colors.white,
+                ),
+              ),
+              style: TextButton.styleFrom(
+                backgroundColor: const Color(0xFFF8485A),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 20,
+                  vertical: 10,
+                ),
+              ),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding:
+                  const EdgeInsets.only(top: 45.0, left: 10.0, right: 20.0),
+              child: Row(
+                children: [
+                  IconButton(
+                    icon: Icon(
+                      Icons.arrow_back,
+                      color: Color(0xff3C270B),
+                    ),
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/journal');
+                    },
+                  ),
+                  Expanded(
+                    child: Align(
+                      alignment: Alignment.center,
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 100),
+                        child: Text(
+                          formatDate(date),
+                          style: TextStyle(
+                            color: Color(0xff666159),
+                            fontFamily: 'Nunito',
+                            fontSize: 20,
+                            fontWeight: FontWeight.w900,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 30.0, top: 30),
+              child: Row(
+                children: [
+                  Image.asset(
+                    getMoodImage(moodRating),
+                    width: 50,
+                    height: 50,
+                  ),
+                  SizedBox(width: 20),
+                  Text(
+                    title,
+                    style: TextStyle(
+                      color: Color(0xff666159),
+                      fontFamily: 'Nunito',
+                      fontSize: 20,
+                      fontWeight: FontWeight.w900,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(30),
+              child: Text(
+                content,
+                style: TextStyle(
+                  fontFamily: 'Roboto',
+                  fontSize: 13,
+                  color: Color(0xff666159),
+                ),
+              ),
+            ),
+            SizedBox(height: 200), // Spacer between content and buttons
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton.icon(
+                  onPressed: () {
+                    Navigator.pushNamed(
+                      context,
+                      '/editJournal',
+                      arguments: {
+                        'title': title,
+                        'content': content,
+                        'entryDate': date,
+                        'moodRating': moodRating,
+                        'id': id,
+                      },
+                    );
+                  },
+                  icon: Icon(Icons.edit),
+                  label: const Text(
+                    'Edit',
+                    style: TextStyle(
+                      fontFamily: 'Inter',
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    foregroundColor: Colors.white,
+                    backgroundColor: Color(0xFF9B968E),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 20,
+                      vertical: 10,
+                    ),
+                  ),
+                ),
+                ElevatedButton.icon(
+                  onPressed: _showDeleteDialog,
+                  icon: Icon(Icons.delete),
+                  label: const Text(
+                    'Delete',
+                    style: TextStyle(
+                      fontFamily: 'Inter',
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    foregroundColor: Colors.white,
+                    backgroundColor: Color(0xFFF8485A),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 20,
+                      vertical: 10,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 50), // Bottom padding for better visual separation
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ frontend/lib/pages/journal_list_page.dart
@@ -0,0 +1,226 @@
+import 'package:flutter/material.dart';
+import 'package:frontend/component/journal_card.dart';
+import 'package:frontend/component/month_dropdown.dart';
+import 'package:frontend/services/journal_service.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class JournalListPage extends StatefulWidget {
+  const JournalListPage({Key? key}) : super(key: key);
+
+  @override
+  State<JournalListPage> createState() => _JournalListPageState();
+}
+
+class _JournalListPageState extends State<JournalListPage> {
+  late JournalService _journalService;
+  late String _selectedMonth;
+  late String _selectedYear;
+  List<dynamic> _journals = [];
+  bool _isLoading = true;
+  String _token = '';
+
+  final List<String> _months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+  ];
+
+
+   @override
+  void initState() {
+    super.initState();
+    _journalService = JournalService();
+    DateTime now = DateTime.now();
+    _selectedMonth = _months[now.month - 1];
+    _selectedYear = now.year.toString();
+    _loadTokenAndFetchJournalEntries();
+  }
+
+
+  Future<void> _loadTokenAndFetchJournalEntries() async {
+    await _loadTokenFromSharedPreferences();
+    if (_token.isNotEmpty) {
+      int month = _months.indexOf(_selectedMonth) + 1;
+      int year = int.parse(_selectedYear);
+      await _fetchJournalEntries(month: month, year: year);
+    } else {
+      setState(() {
+        _isLoading = false;
+      });
+    }
+  }
+
+  Future<void> _loadTokenFromSharedPreferences() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _token = prefs.getString('token') ?? '';
+    });
+  }
+
+  // Future<void> _fetchJournalEntries() async {
+  //   setState(() {
+  //     _isLoading = true;
+  //   });
+  //   try {
+  //     print('token: $_token');
+  //     final entries = await _journalService.getJournalEntries(_token);
+  //     setState(() {
+  //       _journals = entries;
+  //       _isLoading = false;
+  //     });
+  //   } catch (e) {
+  //     print('Failed to fetch journal entries: $e');
+  //     setState(() {
+  //       _isLoading = false;
+  //     });
+  //   }
+  // }
+  Future<void> _fetchJournalEntries({required int month, required int year}) async {
+    setState(() {
+      _isLoading = true;
+    });
+    try {
+      print('Fetching entries for month: $month, year: $year with token: $_token');
+      final entries = await _journalService.getJournalEntriesByDate(_token, month, year);
+      if (entries != null) {
+        setState(() {
+          _journals = entries;
+          _isLoading = false;
+        });
+      } else {
+        setState(() {
+          _isLoading = false;
+      });
+      }
+    } catch (e) {
+      print('Failed to fetch journal entries: $e');
+      setState(() {
+        _isLoading = false;
+      });
+    }
+  }
+
+  void _onMonthYearChanged(int month, int year) {
+    setState(() {
+      _selectedMonth = _months[month - 1];
+      _selectedYear = year.toString();
+    });
+    _fetchJournalEntries(month: month, year: year);
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFFEFBF6),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const SizedBox(
+                height: 60,
+              ),
+              const Text(
+                'My journal',
+                style: TextStyle(
+                  fontFamily: 'Nunito',
+                  fontWeight: FontWeight.w900,
+                  fontSize: 33,
+                  color: Color(0xFF666159),
+                ),
+              ),
+              Row(
+                children: [MonthYearDropdown(onMonthYearChanged: _onMonthYearChanged)],
+              ),
+              const SizedBox(
+                height: 90,
+              ),
+              _isLoading
+                  ? const CircularProgressIndicator()
+                  : _journals.isEmpty
+                      ? Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Image.asset(
+                              'assets/images/journal_empty.png',
+                              width: 208,
+                              height: 208,
+                            ),
+                            const SizedBox(
+                              height: 30,
+                            ),
+                            const Text(
+                              'You haven\'t written any journal yet',
+                              style: TextStyle(
+                                fontFamily: 'Roboto',
+                                fontSize: 15,
+                                color: Color(0xFF666159),
+                              ),
+                            ),
+                            const Text(
+                              'Let\'s start by adding your first journal!',
+                              style: TextStyle(
+                                fontFamily: 'Roboto',
+                                fontSize: 15,
+                                color: Color(0xFF666159),
+                              ),
+                            ),
+                          ],
+                        )
+                      : Column(
+                          children: _journals.map((entry) {
+                            return JournalCard(
+                              id: entry['journal_id'],
+                              title: entry['title'],
+                              content: entry['content'],
+                              entryDate: entry['entry_date'],
+                              moodRating: entry['mood_rating'],
+                            );
+                          }).toList(),
+                        ),
+              const SizedBox(height: 30),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 30.0),
+                child: SizedBox(
+                  width: 250,
+                  child: ElevatedButton.icon(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/newJournal');
+                    },
+                    icon: const Icon(Icons.add),
+                    label: const Text(
+                      'Add daily journal',
+                      style: TextStyle(
+                          fontFamily: 'Inter',
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      foregroundColor: Colors.white,
+                      backgroundColor: const Color(0xFF3C270B),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 20, vertical: 10),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
